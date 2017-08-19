904a4c8ec24119461de0c75475f14afb48b158e4
taeiim <ti535666@gmail.com>
2017-08-19 22:47:14 +0900

    create graph activity

---
+++ Graph/app/src/main/java/com/example/parktaeim/graph/GraphActivity_Alcohol.java
@@ -0,0 +1,13 @@
+package com.example.parktaeim.graph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GraphActivity_Alcohol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alcohol_graph);
+    }
+}
---
+++ Graph/app/src/main/java/com/example/parktaeim/graph/GraphActivity_Tobacco.java
@@ -0,0 +1,17 @@
+package com.example.parktaeim.graph;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by parktaeim on 2017. 8. 19..
+ */
+
+public class GraphActivity_Tobacco extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tobacco_graph);
+    }
+}
